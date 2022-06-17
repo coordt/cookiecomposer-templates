9de5f01251e79a6e776f4d9c6057eb26265641e3
Corey Oordt <coreyoordt@gmail.com>
2022-06-17 10:02:32 -0500

    Changed changelog generation to generate-changelog

--- cookiecutter-boilerplate/{{ cookiecutter.project_name }}/tools/gen-codeowners.sh
+++ cookiecutter-boilerplate/{{ cookiecutter.project_name }}/tools/gen-codeowners.sh
@@ -7,15 +7,15 @@
 # Usage: gen-codeowners.sh path1 [path2 ...]
 
 owners(){
-  for f in $(git ls-files $*); do
-    LINECOUNT=$(wc -l $f | awk '{print $1}')
+  for f in $(git ls-files "$*"); do
+    LINECOUNT=$(wc -l "$f" | awk '{print $1}')
     if [[ $LINECOUNT -gt 0 ]]; then
       echo -n "$f "
       # author emails if loc distribution >= 30%
       git fame -esnwMC --incl "$f" | tr '/' '|' \
-        | awk -v filename=$f -F '|' '(NR>6 && $6>=30) {print $2}' \
+        | awk -v filename="$f" -F '|' '(NR>6 && $6>=30) {print $2}' \
         | xargs echo
     fi
   done
 }
-owners $* | tee CODEOWNERS
+owners "$*" | tee CODEOWNERS
